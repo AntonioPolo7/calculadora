02e8a464e4e3bec25eb1dcb686c1f7c539da0b9a
aitorpellicer <aitortiki@hotmail.com>
2016-03-01 16:45:04 +0100

    aitorpellicer porcentages
    devuelve el numero al aplicar un porcentage

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -14,10 +14,25 @@ public class Calculadora {
     /**
      * @param args the command line arguments
      */
+     
     private static void mensaje(){
         System.out.println ("Trabajamos aquí");
     }
+    private static void Porcentage(){
+     Scanner leer = new Scanner(System.in);
+    double porcen;
+    double num, total;
+    System.out.println("Introduce un porcentage");
+        porcen = leer.nextInt();
+    System.out.println("Introduce un numero para calcular el porcentage");
     
+        num = leer.nextInt();
+    
+        total = (porcen * num)/100;
+    
+    System.out.println("El numero es: "+total);
+    
+    }
     
     public static void main(String[] args) {
         // TODO code application logic here
